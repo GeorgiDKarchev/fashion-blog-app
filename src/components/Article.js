be3b35d52dc3aef2bd76c54726d13105f7e5a729
[Georgi Karchev] <georgikarcev@gmail.com>
2024-01-23 23:43:49 -0500

    Adding additional rticle with image

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,27 +1,50 @@
 import React from "react"
 import imageOne from '../pictures/blog-image-1.jpeg';
+import imageTwo from '../pictures/blog-image-2.jpeg';
 function Article() {
     return (
         <article>
-           <h5>11/12/20</h5>
-           <h2>On the Streets in Brooklyn</h2>
-           <img src={imageOne}
-               alt=" "
-               style ={{ width:"85%"}}
-            />
-            <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam maiores
-        sunt ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
-        incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Nam maiores sunt
-        ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
-        incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Nam maiores sunt
-        ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
-        incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo?
-      </p>
-        </article> 
-    
+            <div className="articleOne">
+                <h5>11/12/20</h5>
+                <h2>On the Streets in Brooklyn</h2>
+                <img src={imageOne}
+                    alt=" "
+                    style={{ width: "85%", height: "10%" }}
+                />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam maiores
+                    sunt ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
+                    incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Nam maiores sunt
+                    ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
+                    incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Nam maiores sunt
+                    ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
+                    incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo?
+                </p>
+            </div>
+
+            <div className="articleOne">
+                <h5>11/11/20</h5>
+                <h2>Vintage in Vogue</h2>
+                <img src={imageTwo}
+                    alt=" "
+                    style={{ width: "85%", height:"10%" }}
+                />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam maiores
+                    sunt ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
+                    incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Nam maiores sunt
+                    ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
+                    incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Nam maiores sunt
+                    ipsam a tenetur exercitationem. Culpa distinctio quia aliquam, unde
+                    incidunt ipsum accusamus, sint quae odit magnam tempora, ea quo?
+                </p>
+            </div>
+        </article>
+
     );
 }
 export default Article
